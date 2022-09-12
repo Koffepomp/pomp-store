acfabe9904059db8fbf39f08c6f0d874985e5828
Koffepomp <Zephs1986@gmail.com>
2022-09-12 09:23:50 +0200

    Update Program.cs

--- Store/Program.cs
+++ Store/Program.cs
@@ -1,9 +1,41 @@
-﻿// Welcome user to the app
-Console.WriteLine(" -----------------------------");
-Console.WriteLine("|| WELCOME TO THE POMP STORE ||");
-Console.WriteLine(" -----------------------------");
-// Show available products
+﻿// Create list with stock
+string[] stock =
+{
+    "Red Bull",
+    "Bulle",
+    "Fisksås",
+    "Saftsoppa",
+    "Ostmacka"
+};
+// Create list with winkelwagen content
+string[] winkelwagen =
+{
+
+};
+
+// Create list with user inventory
+string[] userInventory =
+{
+
+};
+
+// Create bool that keeps the store open
+bool running = true;
+
+while (running)
+{
+    // Clear screen and welcome user to the store
+    WelcomeMessage();
+
+    // Show available products
+    ShowStock();
+    Console.Write("\nWhat do you want to buy? ");
+
+    string userInput = Console.ReadLine();
+
+}
 // Show what is in the winkelwagen
+
 // Ask the user what they want to buy or bail from the store
 // IF the product the user want is in stock
 // Put the product in the shopping cart
@@ -11,62 +43,18 @@ Console.WriteLine(" -----------------------------");
 // ELSE
 // Tell the user that the product is not available
 
-string[] warehouse =
+void WelcomeMessage()
 {
-    "old cheese",
-    "stanky fish"
-};
-string[] shoppingCart =
-{
-
-};
-string[] userInventory = {
-
-};
-
-Print("Welcome to my Gross Store!");
-Print("Here's whats in the store: ");
-
-PrintWarehouse();
-
-string userInput = Console.ReadLine();
-
-if (IsProductAvailable(userInput))
-{
-    PutProductInShoppingCart(userInput);
-}
-else
-{
-    Print("The product you are looking for doesn't exist...");
+    Console.Clear();
+    Console.WriteLine(" -----------------------------");
+    Console.WriteLine("|| WELCOME TO THE POMP STORE ||");
+    Console.WriteLine(" -----------------------------\n\n");
 }
 
-
-// METHODS
-
-void PutProductInShoppingCart(string product)
+void ShowStock()
 {
-
-}
-
-bool IsProductAvailable(string product)
-{
-    foreach (string thing in warehouse)
+    foreach (string product in stock)
     {
-        if (thing == product)
-            return true;
+        Console.WriteLine("- " + product);
     }
-    return false;
-}
-
-void PrintWarehouse()
-{
-    foreach (string ware in warehouse)
-    {
-        Console.WriteLine(" - " + ware);
-    }
-}
-
-void Print(string msg)
-{
-    Console.WriteLine(msg);
 }
